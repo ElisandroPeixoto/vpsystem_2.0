78ec3610e72a1c22bc9692bd3859efc37af60c57
Elisandro <elisandropeixoto21@gmail.com>
2023-01-16 21:22:10 -0300

    feito reles50

--- classes.py
+++ classes.py
@@ -1,22 +1,18 @@
 from funcoes import neutro
 
 
-class Rele50:
-    def __init__(self, corrente_ia, corrente_ib, corrente_ic):
+class Disjuntor:
+    def __init__(self, corrente_ia, corrente_ib, corrente_ic, estado=False):
         self.__corrente_ia = corrente_ia
         self.__corrente_ib = corrente_ib
         self.__corrente_ic = corrente_ic
         self.__corrente_in = neutro(corrente_ia, corrente_ib, corrente_ic)
-
-
-class Disjuntor(Rele50):
-    def __init__(self, corrente_ia, corrente_ib, corrente_ic, estado=False):
-        super().__init__(corrente_ia, corrente_ib, corrente_ic)
         self.__estado = estado
 
     # Habilita acesso ao estado do disjuntor
     @property
     def estado(self):
+        "Abertura e fechamento do disjuntor"
         return self.__estado
 
     def abrir(self):
@@ -28,3 +24,23 @@ class Disjuntor(Rele50):
         "Método de Fechamento"
         self.__estado = True
         return self.__estado
+
+    def rele50(self, pickup):
+        "Relé 50 de Fase"
+        if (self.__corrente_ia[0] or self.__corrente_ib[0] or self.__corrente_ic[0]) > 0:
+            return True
+        else:
+            return False
+
+    def rele50N(self, pickup):
+        "Relé 50 de Neutro"
+        if (self.__corrente_in[0]) > pickup:
+            return True
+        else:
+            return False
+
+    def rele51(self, pickup, curva, dial):
+        pass
+
+    def rele51N(self, pickup, curva, dial):
+        pass
--- testes_unitarios.py
+++ testes_unitarios.py
@@ -6,15 +6,38 @@ from classes import Disjuntor
 class TestesFuncoes(unittest.TestCase):
 
     def test_neutro_zero(self):
+        "Retorna valor de neutro igual a zero"
         self.assertEqual(neutro([120, 0], [120, -120], [120, 120]), (0, 0))
 
     def test_disjuntor_fechar(self):
+        "Fechamento do disjuntor"
         dj = Disjuntor([0, 0], [0, 0], [0, 0])
         self.assertTrue(dj.fechar())
 
     def test_disjuntor_abrir(self):
+        "Abertura do Disjuntor"
         dj = Disjuntor([0, 0], [0, 0], [0, 0], True)
         self.assertFalse(dj.abrir())
+
+    def test_funcao_50(self):
+        "Atuacao funcao 50"
+        dj = Disjuntor([100, 0], [50, -120], [50, 120])
+        self.assertTrue(dj.rele50(70))
+
+    def test_funcao_50_2(self):
+        "Nao atuacao da funcao 50"
+        dj = Disjuntor([0, 0], [0, 0], [0, 0])
+        self.assertFalse(dj.rele50(100))
+
+    def test_funcao_50N(self):
+        "Atuacao do 50 de Neutro"
+        dj = Disjuntor([100, 0], [0, 0], [0, 0])
+        self.assertTrue(dj.rele50N(90))
+
+    def test_funcao_50N_2(self):
+        "Nao atuacao da funcao 50"
+        dj = Disjuntor([0, 0], [0, 0], [1200, 0])
+        self.assertFalse(dj.rele50N(1500))
 
 
 if __name__ == "__main__":
